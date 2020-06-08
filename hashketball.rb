d47c2d60b1aace6719110ea3ea4a29094bb4014a
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-08 20:57:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash
   hasketball = {
     :home => {
-      :team_name =>,
-      :colors => [],
+      :team_name => 'Brooklyn Nets',
+      :colors => ['Black', 'White'],
       :players => [
         {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks =>}
         ]
